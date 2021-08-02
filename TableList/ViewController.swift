b81028a5f8a56b9069f95e067e1e3b27d2eddc86
lazdovska <lazdovska.kristine@gmail.com>
2021-08-02 18:55:53 +0300

    Initial Commit

---
+++ TableList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableList
+//
+//  Created by kristine.lazdovska on 02/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
